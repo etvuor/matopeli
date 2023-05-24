94cda5cc6637169e80e73ff43eda948ad2263bd3
etvuor <73583144+etvuor@users.noreply.github.com>
2023-05-24 08:38:32 +0300

    Update mod.rs

--- src/unit/player_builder/mod.rs
+++ src/unit/player_builder/mod.rs
@@ -1,41 +1,35 @@
-use super::Player;
+use crate::unit::player::Player;
+use crate::unit::player::position as player;
 
 pub struct PlayerBuilder {
-    health: u8,
-    speed: f64,
-    position: (u32, u32),  // Add the position field of type (u32, u32)
+    health: u32,
+    position: (u32, u32),
 }
 
 impl PlayerBuilder {
     pub fn new() -> Self {
-        Self {
+        PlayerBuilder {
             health: 100,
-            speed: 1.0,
-            position: (0, 0),  // Set the initial position
+            position: (0, 0),
         }
     }
 
-    pub fn health(mut self, health: u8) -> Self {
+    pub fn health(&mut self, health: u32) -> &mut Self {
         self.health = health;
         self
     }
 
-    pub fn speed(mut self, speed: f64) -> Self {
-        self.speed = speed;
-        self
-    }
-
-    pub fn position(mut self, position: (u32, u32)) -> Self {
+    pub fn position(&mut self, position: (u32, u32)) -> &mut Self {
         self.position = position;
         self
     }
 
-    pub fn build(self) -> Player {
-        Player {
-            health: self.health,
-            speed: self.speed,
-            position: self.position,
-        }
+    pub fn build(&self) -> Player<f64> {
+        Player::new(
+            self.health,
+            player::Position::new(self.position.0.into(), self.position.1.into()),
+        )
     }
 }
+
 
