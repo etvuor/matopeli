a3f1cac7c4e7f93feb81bfd26608b4987fdb1b2c
etvuor <73583144+etvuor@users.noreply.github.com>
2023-05-25 08:21:35 +0300

    Update player.rs

--- src/unit/player.rs
+++ src/unit/player.rs
@@ -1,53 +1,15 @@
-use rand::Rng;
-use num_traits::NumAssign;
-use std::ops::Range;
-use num_traits::{NumAssign, Num};
-
-// Define a generic Position struct
-#[derive(Debug, Clone, Copy)]
-pub struct Position<T> {
-    pub x: T,
-    pub y: T,
-}
-
-// Implement the Position trait for the generic Position struct
-impl<T> Position<T> {
-    pub fn position(&self) -> Position<T> {
-        *self
-    }
-}
-
-// Implement PartialEq for Position struct
-impl<T: PartialEq> PartialEq for Position<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y
-    }
-}
-
-// Player struct
+#[derive(Default)]
 pub struct Player {
-    pub position: Position<f64>,
     pub speed: f64,
     pub health: u32,
 }
 
 impl Player {
-    pub fn new(x: f64, y: f64, speed: f64, health: u32) -> Self {
-        Player {
-            position: Position { x, y },
-            speed,
-            health,
+    pub fn new() -> Self {
+        Self {
+            speed: 0.0,
+            health: 0,
         }
-    }
-
-    pub fn position(&self) -> Position<f64> {
-        self.position
-    }
-
-    pub fn set_rand_position(&mut self, rng: &mut impl Rng, x_range: Range<f64>, y_range: Range<f64>) {
-        let x = rng.gen_range(x_range);
-        let y = rng.gen_range(y_range);
-        self.position = Position { x, y };
     }
 
     pub fn is_alive(&self) -> bool {
@@ -70,4 +32,5 @@ impl Player {
         self.speed
     }
 }
+
 
